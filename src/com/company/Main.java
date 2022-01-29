9918784eba09819149c8a267419bf535793d59d7
Notnik_KG <tasnim0800@gmail.com>
2022-01-29 14:13:51 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        method(8.45d);
+
+    }
+    static void method (double a) {
+        int b = (int)  a;
+        System.out.println(b);
+
+    }
+}
